3e25dbef4d607921146916bf199819f1a9ff5b17
luiscascelli <luis.cascelli@gmail.com>
2020-10-08 16:11:04 -0300

    ac3

---
+++ primos.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def nprimos():
+    p = [1, 2]
+    i = 3
+    while len(p) < 100:
+        primo = True
+        for j in p[1:]:
+            if i % j == 0:
+                primo = False
+                break
+        if primo:
+            p.append(i)
+        i += 1
+    return p
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
